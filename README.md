fdaa14d3b345d57c6d89af2bacc2fa2d8ceadd17
swarnika611 <63455075+swarnika611@users.noreply.github.com>
2022-07-08 21:49:15 +0530

    Completed

---
+++ README.md
@@ -0,0 +1,5 @@
+Steps
+1-Go to the terminal install dependences by running command -- npm install
+this command will install all the dependences and packages needed to run this app
+2-Run the app by running the commmand --npm start
+this commmand will run the app
---
+++ src/components/AddSlots.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class AddSlots extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>Welcome To Parking Area</h2>
+        <button id="Add-slot">Add Slots</button>
+      </div>
+    );
+  }
+}
+
+export default AddSlots;
